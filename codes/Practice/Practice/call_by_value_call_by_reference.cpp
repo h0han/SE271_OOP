98bcd1a9e85f5d417e8caf8c94de489fa6cc4521
h0han <hyhan@dgist.ac.kr>
2020-10-10 21:09:08 +0900

    practices

--- codes/Practice/Practice/call_by_value_call_by_reference.cpp
+++ codes/Practice/Practice/call_by_value_call_by_reference.cpp
@@ -20,20 +20,20 @@ void swap3(int& num1, int& num2) {
 	num2 = tmp;
 }
 
-int main() {
-	int num1 = 1;
-	int num2 = 3;
-	int num3 = 1;
-	int num4 = 3;
-	cout << num1 << " " << num2 << endl;
-	swap1(num1, num2);
-	cout << "swap1 " << num1 << num2 << endl;
-	cout << num1 << " " << num2 << endl;
-	swap2(&num1, &num2);
-	cout << "swap2 " << num1 << num2 << endl;
-	cout << num3 << " " << num4 << endl;
-	swap3(num3, num4);
-	cout << "swap3 " << num3 << num4 << endl;
-
-	return 0;
-}
+//int main() {
+//	int num1 = 1;
+//	int num2 = 3;
+//	int num3 = 1;
+//	int num4 = 3;
+//	cout << num1 << " " << num2 << endl;
+//	swap1(num1, num2);
+//	cout << "swap1 " << num1 << num2 << endl;
+//	cout << num1 << " " << num2 << endl;
+//	swap2(&num1, &num2);
+//	cout << "swap2 " << num1 << num2 << endl;
+//	cout << num3 << " " << num4 << endl;
+//	swap3(num3, num4);
+//	cout << "swap3 " << num3 << num4 << endl;
+//
+//	return 0;
+//}
