f066e847952ebfb79df1b50b960a3642fdd711f3
h0han <hyhan@dgist.ac.kr>
2020-10-25 05:07:41 +0900

    Assignment_02

---
+++ assignments/assignment_02/Assignment2/Project1/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+enum class DataType { Integer, Float, Str, NotAvailable };
+
+int main() {
+	auto a = 1;
+	cout << a.type
+}
--- assignments/assignment_02/hw2_skeleton/hw2.cpp
+++ assignments/assignment_02/hw2_skeleton/hw2.cpp
@@ -7,12 +7,20 @@
 #include "hw2.h"
 
 #include <iostream>
+using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 // You may also want to have additional functions, 
 // e.g., static functions or forward declaration of functions, Then
 //
 // IMPLEMENT HERE
+struct node {   // node structure은 data region과 next region을 포함.
+    int idata; 
+    float fdata;
+    string sdata;
+    node* next;
+};
+
 //
 // NOTE: DO NOT USE global, static variables
 ////////////////////////////////////////////////////////////////////////////////
@@ -20,9 +28,17 @@
 // Constructors
 VariableList::VariableList() {
     // IMPLEMENT HERE
+    head = NULL;
+    tail = NULL;
+    size = 0;
 }
-VariableList::VariableList(const int* initialArray, const int size) {
+VariableList::VariableList(const int* initialArray, const int size) {   // array에 있는 elem의 node화 필요
     // IMPLEMENT HERE
+    int i;  // for loop
+    for (i = 0; i < size; ++i) {
+
+    }
+    
 }
 VariableList::VariableList(const float* initialArray, const int size) {
     // IMPLEMENT HERE
@@ -33,70 +49,148 @@ VariableList::VariableList(const std::string* initialArray, const int size) {
 
 // Destructor
 // Note: Please delete(free) the memory you allocated 
-VariableList::~VariableList() {
+VariableList::~VariableList() { //소멸자가 불릴 때 전체 node 삭제?
     // IMPLEMENT HERE
 }
 
 // Member functions
 // add: Add the value at the end of the list
 void VariableList::add(const int val) {
-    // IMPLEMENT HERE
+     //IMPLEMENT HERE
+    node* tmp = new node;
+    tmp->idata = val;
+    tmp->next = NULL;
+
+    if (head == NULL) {
+        head = tmp;
+        tail = tmp;
+    }
+    else {
+        tail->next = tmp;
+        tail = tail->next;
+    }
+    ++size;
+    delete tmp;
 }
 void VariableList::add(const float val) {
     // IMPLEMENT HERE
+    node* tmp = new node;
+    tmp->fdata = val;
+    tmp->next = NULL;
+
+    if (head == NULL) {
+        head = tmp;
+        tail = tmp;
+    }
+    else {
+        tail->next = tmp;
+        tail = tail->next;
+    }
+    ++size;
+    delete tmp;
 }
 void VariableList::add(const std::string& val) {
     // IMPLEMENT HERE
+    node* tmp = new node;
+    string str(val);
+    tmp->sdata = str;
+    tmp->next = NULL;
+
+    if (head == NULL) {
+        head = tmp;
+        tail = tmp;
+    }
+    else {
+        tail->next = tmp;
+        tail = tail->next;
+    }
+    ++size;
+    delete tmp;
 }
 
 // append: Copy all values of varList and append them at the end of the list
 void VariableList::append(const VariableList& varList) {
     // IMPLEMENT HERE
+    int list_size = varList.getSize();
+    int i;  // for loop
+
+    node* tmp = new node;
+    for (i = 0; i < list_size; ++i) {
+        
+    }
 }
 
 // replace: replace the value at the given index in the list
 bool VariableList::replace(const int idx, const int val) {
     // IMPLEMENT HERE
+    int step = 0;
+    if (idx < 0 | idx > size) {
+        return false;
+    }
+    while (step == idx) {
+
+    }
     return false; // CHANGE HERE
 }
 bool VariableList::replace(const int idx, const float val) {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return false; // CHANGE HERE
 }
 bool VariableList::replace(const int idx, const std::string& val) {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return false; // CHANGE HERE
 }
 
 // remove: remove the item at the given index in the list
 bool VariableList::remove(const int idx) {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return false; // CHANGE HERE
 }
 
 // getSize: return the number of elements of the List
 unsigned int VariableList::getSize() const {
     // IMPLEMENT HERE
-    return 0; // CHANGE HERE
+    return size; // CHANGE HERE
 }
 
 // getType: return the data type for the value at the given index
 DataType VariableList::getType(const int idx) const {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return DataType::NotAvailable; // CHANGE HERE
 }
 
 // getValue: copy the value to the variable
 bool VariableList::getValue(const int idx, int& val) const {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return false; // CHANGE HERE
 }
 bool VariableList::getValue(const int idx, float& val) const {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return false; // CHANGE HERE
 }
 bool VariableList::getValue(const int idx, std::string& val) const {
     // IMPLEMENT HERE
+    if (idx < 0 | idx > size) {
+        return false;
+    }
     return false; // CHANGE HERE
 }
 
